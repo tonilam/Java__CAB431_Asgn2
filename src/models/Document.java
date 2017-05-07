a51764140a1a0d0efc01a6b3592c74aefc57971e
Toni Lam <qut.toni.9516778@gmail.com>
2017-05-07 23:58:30 +1000

    Create some models base on given dataset

---
+++ src/models/Document.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package models;
+
+/**
+ * @author Toni Lam
+ *
+ * @since 1.0
+ * @version 1.0, May 7, 2017
+ */
+public class Document {
+	public int newsitem_itemid;
+	public String newsitem_id;
+	public String newsitem_date;
+	
+	public String title;
+	public String headline;
+	public String dateline;
+	public String text;
+	public String copyright;
+
+	/* ignore metadata */
+}
---
+++ src/models/RelevanceJudgment.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package models;
+
+/**
+ * @author Toni Lam
+ *
+ * @since 1.0
+ * @version 1.0, May 7, 2017
+ */
+public class RelevanceJudgment {
+	public String topicId;
+	public int docId;
+	public boolean relevance;
+}
